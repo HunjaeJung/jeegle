63ac08c1d25197041f25d16ad2193669263a55cb
Wonkyung Lyu <me@lyuwonkyung.com>
2015-03-14 03:10:49 +0900

    1. 컬렉션 일부 추가 2. 서버 메소드 프로토타입 정의

--- server/main.js
+++ server/main.js
@@ -8,10 +8,27 @@ if (Meteor.isServer) {
         // 클라이언트에서 호출 가능한 server-side 함수들을 정의합니다.
         Meteor.methods({
 
+            // 사용자가 문구를 넣으면 형태소를 분석하여 알맞은 이미지셋을 images 컬렉션에 담아 publish 해줍니다.
+            searchImageByText: function (text) {
+
+
+                // text를 형태소 분석하여 중요 단어를 추출합니다.
+
+                // 서버에 저장된 images 컬렉션에서 tag 중 알맞은 image를 찾아냅니다.
+
+                // 해당 이미지들을 중요도에 따라 images 컬렉션에 담아 publish 합니다.
+
+            },
+
+            // 사용자가 최종 작업물을 서버에 게시합니다.
+            saveWorkingOutputs: function () {
+
+            }
+
+
 
 
         });
 
     })
-
 }
